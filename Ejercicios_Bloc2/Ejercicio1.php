5c6318a9b9b05044aa369f1aeefc5d03958de7ba
Valentin Ruiz Lopez <alurui180@ieselcaminas.org>
2020-10-06 12:32:22 +0200

    Bug

--- Ejercicios_Bloc2/Ejercicio1.php
+++ Ejercicios_Bloc2/Ejercicio1.php
@@ -1,17 +1,49 @@
+<style>
+    td {
+        border: 1px solid grey;
+        padding: 5px;
+    }
+</style>
+
 <?php
+    const ROWLENGHT = 11;
+    const COLLENGHT = 10;
 
-const ROWLENGHT = 11;
-const COLLENGHT = 10;
+    $row = 23;
+    $col = 1;
 
-// [row][col]
-$table[
-    []
-];
-$row = 23;
-$col = 1;
-$first = true;
-
-//for ($i = ROWLENGHT; )
-
+    $rowDiv = 22;
+    $colDiv = 1;
 
 ?>
+
+    <table class="table">
+        <tr>
+        <td>/</td>
+
+<?php
+    for ($i = 0; $i < ROWLENGHT; $i++) {
+        echo '<td>'.$row.'</td>';
+        $row++;
+    }
+
+    for ($i = 0; $i < COLLENGHT; $i++) {
+        echo "<tr>";
+        echo "<td>".$col."</td>";
+        $col++;
+
+        for ($j = 0; $j < ROWLENGHT; $j++) {
+            if ($rowDiv % $colDiv == 0) {
+                echo "<td>*</td>";
+            } else {
+                echo "<td>-</td>";
+            }
+            $rowDiv++;
+        }
+        echo "</tr>";
+        $colDiv++;
+    }
+
+?>
+        </tr>
+    </table>
