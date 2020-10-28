1dce7433fbda633c7d3804186b550dafabd158af
Valentin Ruiz Lopez <alurui180@ieselcaminas.org>
2020-10-28 12:05:22 +0100

    init blog

---
+++ blog/guardar.php
@@ -0,0 +1,15 @@
+<?php
+    $titulo = $_POST['title'];
+    $description = $_POST['description'];
+    $path = "./posts/$titulo";
+
+    $data = $titulo.";".$description;
+
+    if (file_put_contents($path, $data)) {
+        log("Guardado");
+        header('location: index.php');
+    } else {
+        log("No guardado");
+        header('location: noguardado.php');
+    }
+?>
