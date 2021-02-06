7945944ec6be8e812126d36c34048850791ad512
Valentin Ruiz Lopez <alurui180@ieselcaminas.org>
2021-02-06 17:34:30 +0100

    CutreCloud out

--- cutrecloud/components/form_upload.php
+++
@@ -1,8 +0,0 @@
-<form method="post" action="controller.php" enctype="multipart/form-data">
-    <div class="form-inline">
-    <div class="input-group mb-2 mr-sm-2">
-        <input type="file" class="form-control-file" name="inputFile" id="inputFile" placeholder="Input File" aria-describedby="fileHelpId">
-    </div>
-    <button type="submit" name="upload" id="upload" class="btn btn-primary mb-2">Submit</button>
-    </div>
-</form>
